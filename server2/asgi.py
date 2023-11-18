eed4c429793b5a31fd6cd47dbc5a16e19f149c11
Dhritiman Bharadwaj <70242680+dhriti987@users.noreply.github.com>
2023-11-18 16:47:23 +0530

    Update asgi.py

--- server2/asgi.py
+++ server2/asgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server2.settings')
 from django import setup
 setup()
 from django.core.asgi import get_asgi_application
